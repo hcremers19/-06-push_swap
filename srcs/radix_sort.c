078eac59edacaf23855789b4401ae06b559871ed
Hugo Cremers <hcremers@student.s19.be>
2022-03-02 11:59:48 +0100

    push_swap v0.2: radix_sort works with positive numbers but is not optimized

--- srcs/radix_sort.c
+++ srcs/radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: hcremers <hcremers@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 18:54:53 by hcremers          #+#    #+#             */
-/*   Updated: 2022/03/01 19:25:15 by hcremers         ###   ########.fr       */
+/*   Updated: 2022/03/02 11:53:22 by hcremers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,23 @@ void	radix_sort(t_tab *tabs)
 {
 	int	chiffre;
 	int	tournante;
+	int	constant_alen;
 
+	constant_alen = tabs->alen;
 	chiffre = 0;
-	while (chiffre < 8)
+	while (chiffre < 32)
 	{
 		tournante = 0;
-		ft_putstr_fd("chiffre = ", 1); ft_putnbr_fd(chiffre, 1); ft_putstr_fd("\n", 1);
-		while (tournante < tabs->alen)
+		while (tournante < constant_alen)
 		{
-			ft_putstr_fd("-tournante = ", 1); ft_putnbr_fd(tournante, 1); ft_putstr_fd("\n", 1);
-			if (((tabs->a[tabs->alen] >> chiffre) & 1) % 2 == 1)
-			{
+			if ((((tabs->a[tabs->alen - 1] >> chiffre) & 1) % 2) == 1)
 				pb(tabs);
-				ft_putstr_fd("--if\n\n", 1);
-			}
 			else
-			{
 				ra(tabs);
-				ft_putstr_fd("--else\n\n", 1);
-			}
 			tournante++;
 		}
 		while (tabs->blen)
 			pa(tabs);
 		chiffre++;
-		ft_putstr_fd("-----------------\n", 1);
 	}
 }
